b0abcac9e9cec86ce6fcfa1703fef8bd88d5522e
faridasad <asadlifaridd@gmail.com>
2022-09-30 18:15:14 +0400

    Add project files.

---
+++ Parking/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Parking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.entry_button = new System.Windows.Forms.Button();
+            this.totalBox = new System.Windows.Forms.TextBox();
+            this.availableBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.headerText = new System.Windows.Forms.Label();
+            this.exit_button = new System.Windows.Forms.Button();
+            this.detailsBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // entry_button
+            // 
+            this.entry_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.entry_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.entry_button.Location = new System.Drawing.Point(34, 110);
+            this.entry_button.Name = "entry_button";
+            this.entry_button.Size = new System.Drawing.Size(133, 42);
+            this.entry_button.TabIndex = 0;
+            this.entry_button.Text = "WAY IN";
+            this.entry_button.UseVisualStyleBackColor = false;
+            this.entry_button.Click += new System.EventHandler(this.entry_button_Click);
+            // 
+            // totalBox
+            // 
+            this.totalBox.Location = new System.Drawing.Point(555, 118);
+            this.totalBox.Name = "totalBox";
+            this.totalBox.ReadOnly = true;
+            this.totalBox.Size = new System.Drawing.Size(57, 22);
+            this.totalBox.TabIndex = 2;
+            this.totalBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // availableBox
+            // 
+            this.availableBox.Location = new System.Drawing.Point(555, 177);
+            this.availableBox.Name = "availableBox";
+            this.availableBox.ReadOnly = true;
+            this.availableBox.Size = new System.Drawing.Size(57, 22);
+            this.availableBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Uni Sans Book", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(461, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "available";
+            // 
+            // headerText
+            // 
+            this.headerText.AutoSize = true;
+            this.headerText.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.headerText.Font = new System.Drawing.Font("Open Sans Condensed SemiBold", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerText.ForeColor = System.Drawing.Color.White;
+            this.headerText.Location = new System.Drawing.Point(238, 21);
+            this.headerText.Name = "headerText";
+            this.headerText.Size = new System.Drawing.Size(212, 62);
+            this.headerText.TabIndex = 6;
+            this.headerText.Text = "P A R K I N G";
+            this.headerText.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // exit_button
+            // 
+            this.exit_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.exit_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exit_button.Location = new System.Drawing.Point(34, 169);
+            this.exit_button.Name = "exit_button";
+            this.exit_button.Size = new System.Drawing.Size(133, 43);
+            this.exit_button.TabIndex = 7;
+            this.exit_button.Text = "WAY OUT";
+            this.exit_button.UseVisualStyleBackColor = false;
+            this.exit_button.Click += new System.EventHandler(this.exit_button_Click);
+            // 
+            // detailsBox
+            // 
+            this.detailsBox.FormattingEnabled = true;
+            this.detailsBox.ItemHeight = 16;
+            this.detailsBox.Location = new System.Drawing.Point(34, 275);
+            this.detailsBox.Name = "detailsBox";
+            this.detailsBox.Size = new System.Drawing.Size(579, 212);
+            this.detailsBox.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Uni Sans Book", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(461, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "total";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // statusText
+            // 
+            this.statusText.AutoSize = true;
+            this.statusText.BackColor = System.Drawing.Color.Transparent;
+            this.statusText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusText.ForeColor = System.Drawing.Color.IndianRed;
+            this.statusText.Location = new System.Drawing.Point(193, 252);
+            this.statusText.Name = "statusText";
+            this.statusText.Size = new System.Drawing.Size(0, 20);
+            this.statusText.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(45)))), ((int)(((byte)(47)))));
+            this.ClientSize = new System.Drawing.Size(701, 512);
+            this.Controls.Add(this.statusText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.detailsBox);
+            this.Controls.Add(this.exit_button);
+            this.Controls.Add(this.headerText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.availableBox);
+            this.Controls.Add(this.totalBox);
+            this.Controls.Add(this.entry_button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PARKING";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button entry_button;
+        private System.Windows.Forms.TextBox totalBox;
+        private System.Windows.Forms.TextBox availableBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label headerText;
+        private System.Windows.Forms.Button exit_button;
+        private System.Windows.Forms.ListBox detailsBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label statusText;
+    }
+}
+
---
+++ Parking/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parking
+{
+    public partial class Form1 : Form
+    {
+        private int total = 10;
+        private int available = 10;
+        List<int> numbers = new List<int>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            totalBox.Text = Convert.ToString(total);
+            availableBox.Text = Convert.ToString(available);
+        }
+
+
+        private void entry_button_Click(object sender, EventArgs e)
+        {
+            int random = new Random().Next(1, 11);
+
+            if (available == 0)
+            {
+                statusText.Text = "Heç bir boş slot yoxdur!";
+                return;
+            }
+
+            if (numbers.Contains(random))
+            {
+                statusText.Text = $"{random} nömrəli maşın artıq daxil olub!";
+                return;
+            }
+
+            statusText.Text = "";
+            numbers.Add(random);
+            available -= 1;
+            availableBox.Text = Convert.ToString(available);
+            detailsBox.Items.Add(random + "  —  " + DateTime.Now.ToString());
+
+        }
+
+
+        private void exit_button_Click(object sender, EventArgs e)
+        {
+            int random = new Random().Next(1, 11);
+
+
+            if (available < total && !numbers.Contains(random))
+            {
+                statusText.Text = $"{random} nömrəli maşın mövcud deyil!";
+                return;
+            };
+
+
+
+            for (int i = detailsBox.Items.Count - 1; i >= 0; i--)
+            {
+                if (detailsBox.Items[i].ToString().StartsWith(Convert.ToString(random)))
+                {
+                    statusText.Text = "";
+                    detailsBox.Items.RemoveAt(i);
+                    available += 1;
+                    availableBox.Text = Convert.ToString(available);
+                    numbers.Remove(random);
+                }
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
